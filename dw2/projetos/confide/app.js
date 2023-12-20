164ded3a7e7ce4be941501d1a35bfe206d245717
jeffersongurguri <jeffersongurguri@gmail.com>
2023-12-20 18:37:03 -0300

    Update app.js

--- dw2/projetos/confide/app.js
+++ dw2/projetos/confide/app.js
@@ -1,4 +1,4 @@
-let secreto = `"Sistema Operacional que estais na memória,\n
+let textoSecreto = `"Sistema Operacional que estais na memória,\n
 Compilado seja o vosso programa,\n
 Venha à tela os vossos comandos,\n
 Seja executada a nossa rotina,\n
@@ -10,68 +10,95 @@ Não nos deixai entrar em looping,\n
 Mas livrai-nos do Dump,\n
 A main().`
 
-function particionarEmPalavras(texto) {
-    let matriz = []
-    
-    let linhas = texto.split('\n')
-    for (let linha of linhas) {
-        let palavras = linha.split(' ')
-        matriz.push(palavras) // [ ["Mas", "Livrai-nos", "do", "Dump,"] ]
+
+/**
+ * A partir de uma string de múltiplas linhas retorna uma matriz (array de arrays) com strings contendo cada palavra
+ * @param {string} texto - texto a ser particionado em uma matriz de palavras, cada linha numa array.
+ * @returns matriz - um arrays de arrays, onde cada subarray é uma coleção da palavras.
+ */
+function particionarTextoComMatrizDePalavras(texto) {
+    let matriz = [] // 
+    let arrStrLinhas = texto.split('\n') // divida a string utilizando o caracter nova `\n` linha como separador.
+    for (let strLinha of arrStrLinhas) { //para linha em formato de array
+        let strPalavras = strLinha.split(' ') // particione a linha utilizando o caracter de espaço como separado.
+        matriz.push(strPalavras) // Adiciona o array representando a linha atual na matriz.
     }
-    return matriz // vetor de vetores
+    return matriz
 }
 
-function onSegredoMouseEnter(event) {
-    let filhos = this.children
+/**
+ * Callback para responder ao evento de entrada do mouse na div
+ * @param event - Evento
+ */
+function quandoMouseEntrarDiv(event) {
+    let elemNodeFilhos = this.children
 
-    for (let filho of filhos) {
-        if (filho.classList.contains('espaco')) continue;
-        filho.classList.remove('palavra-omitida')
-        filho.classList.add('palavra-revelada')
+    for (let elemNodeFilho of elemNodeFilhos) {
+        if (elemNodeFilho.classList.contains('espaco')) continue; // caso o filho seja um span contendo apenas um espaço, ignore
+        elemNodeFilho.classList.remove('palavra-omitida') // remova a estilização que torna omitida a palavra
+        elemNodeFilho.classList.add('palavra-revelada') // adicione a estilização que torna revelada a palavra
     }
 }
 
-function onSegredoMouseLeave(event) {
-    let filhos = this.children
+/**
+ * Callback para responder ao evento de saída do mouse na div
+ * @param event - Evento
+ * @see quandoMouseEntrarDiv
+ */
+function quandoMouseSaiDiv(event) {
+    let elemNodeFilhos = this.children
 
-    for (let filho of filhos) {
-        if (filho.classList.contains('espaco')) continue;
-        filho.classList.remove('palavra-revelada')
-        filho.classList.add('palavra-omitida')
+    for (let elemNodeFilho of elemNodeFilhos) {
+        if (elemNodeFilho.classList.contains('espaco')) continue; // caso o filho seja um span contendo apenas um espaço, ignore
+        elemNodeFilho.classList.remove('palavra-relevada') // remove a estilização que torna relevada a palavra
+        elemNodeFilho.classList.add('palavra-omitida') // adicione a estilização que torna omitida a palavra
     }
 }
 
-function configurarPalavras(linhas, container) {
-    for (let linha of linhas) {
-        let divnode = document.createElement('div')
-        divnode.classList.add('segredo')
-        
-        divnode.onmouseenter = onSegredoMouseEnter
-        divnode.onmouseleave = onSegredoMouseLeave
+/**
+ * Preenche a div de conteúdo com spans representados as palavras, linha a linha.
+ * @param {*} matStrLinhas 
+ * @param {*} divConteudo 
+ */
+function preencherDivPrincipal(matStrLinhas, divConteudo) {
+    for (let arrStrLinha of matStrLinhas) {
+        // criação de div que representa uma linha do texto
+        let elemNodeDiv = document.createElement('div')
 
-        for (let palavra of linha) {
-            let spannode = document.createElement('span')
-            
-            spannode.classList.add('palavra-omitida')
-            
-            let espaconode = document.createElement('span')
-            
-            espaconode.classList.add('espaco')
-            
-            espaconode.innerText= " "
-            spannode.innerText = palavra
-            
-            divnode.appendChild(spannode)
-            divnode.appendChild(espaconode)
+        // estilização para embelezamento apenas
+        elemNodeDiv.classList.add('segredo')
+
+        // Registrar manipuladores (callbacks) de evento
+        elemNodeDiv.onmouseenter = quandoMouseEntrarDiv
+        elemNodeDiv.onmouseleave = quandoMouseSaiDiv
+
+        for (let strPalavra of arrStrLinha) {
+            // criação de tag span que engloba a palavra.
+            let elemNodeSpanPalavra = document.createElement('span')
+            elemNodeSpanPalavra.classList.add('palavra-omitida')
+
+            // criação de tag span que engloba um espaço em branco, separando as palavras
+            let elemNodeSpanEspaco = document.createElement('span')
+            elemNodeSpanEspaco.classList.add('espaco')
+            elemNodeSpanEspaco.innerText = " "
+            elemNodeSpanPalavra.innerText = strPalavra
+
+            // Adicionar os spans a div que representa a linha
+            elemNodeDiv.appendChild(elemNodeSpanPalavra)
+            elemNodeDiv.appendChild(elemNodeSpanEspaco)
         }
-        container.appendChild(divnode)
+        // Adicionar a div que representa uma linha a div que representa o conteúdo
+        divConteudo.appendChild(elemNodeDiv)
     }
 }
 
+/**
+ * Função Principal
+ */
 function setup() {
-    console.log("running")
-    let mainContent = document.getElementById('main-content')
-    let linhas = particionarEmPalavras(secreto)
-    configurarPalavras(linhas, mainContent)
+    console.log("Executando a aplicação web")
+    let divPrincipal = document.getElementById('main-content')
+    let linhasComoArrayDeSpans = particionarTextoComMatrizDePalavras(textoSecreto)
+    preencherDivPrincipal(linhasComoArrayDeSpans, divPrincipal)
 }
 
